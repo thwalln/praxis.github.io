d808012a3c9799f288ec9850b5dcd12e3a71eed3
Thomas Wallner <thomas.wallner23@gmail.com>
2022-05-04 20:15:31 +0200

    adding map to kontakt

--- public/app.js
+++ public/app.js
@@ -1,3 +1,18 @@
 document.querySelector('header').addEventListener('click', () => {
-  console.log('click');
-});
+  console.log('click')
+})
+
+function initMap() {
+  const farchant = { lat: 47.5287349, lng: 11.111717 }
+
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: farchant,
+  })
+  const marker = new google.maps.Marker({
+    position: farchant,
+    map: map,
+  })
+}
+
+window.initMap = initMap
